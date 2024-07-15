73c0ac51ed74ddb4d617d907239dc80bfe9b5c77
Arpitsoni0503 <arpitsoni77757@gmail.com>
2024-07-15 19:36:30 +0000

    datatypes

---
+++ 01.basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 //dont write code in this from because the code is not just about writing,it also emphasis on code readiability and efficiency
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"//" " ->this is a string
+let age = 18// 18 is a number
+let isLoggedIn = false
+let state;
+/* =>primitive data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value//ye kud mein ek value hai
+// undefined => // u have not defined anything thats the undefined called
+// symbol => unique
+
+*/
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+//null ka type object hai
