68482ca40a9447657de86201fab01afaf20a4176
Arpitsoni0503 <arpitsoni77757@gmail.com>
2024-07-17 11:39:39 +0530

    Update datatypes.js

--- 01.basics/datatypes.js
+++ 01.basics/datatypes.js
@@ -7,7 +7,11 @@ console.log(3 //dont write code in this from because the code is not just about 
      3) // code readability should be high
 
 console.log("Hitesh")
-
+let a=10
+let b="22"
+let c=10
+console.log(a+c+b)
+console.log()
 
 let name = "hitesh"//" " ->this is a string
 let age = 18// 18 is a number
@@ -15,8 +19,9 @@ let isLoggedIn = false
 let state;
 /* =>primitive data types
 // number => 2 to power 53
-// bigint
-// string => ""
+// bigint 
+// string => "" also strings reads the whole word in quotes( "ws1234@@")->so this is a complete one string
+// example of string is **PASSWORD**
 // boolean => true/false
 // null => standalone value//ye kud mein ek value hai
 // undefined => // u have not defined anything thats the undefined called
